c1b13d84f20a5e3a03a31f29fab4d2344bbe893a
pinponkun <hayashidareki0423@gmail.com>
2025-06-17 16:50:06 +0900

    ABC001_A.cs

---
+++ ABC001_A.cs
@@ -0,0 +1,11 @@
+class ABC001_A
+{
+    static void Main()
+    {
+        int H1 = int.Parse(Console.ReadLine());
+        int H2 = int.Parse(Console.ReadLine());
+        int A = H1 - H2;
+
+        Console.WriteLine(A);
+    }
+}
