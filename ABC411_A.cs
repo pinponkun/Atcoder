76e7620aa7eee4a3f0e98301b652d00562bd799a
pinponkun <hayashidareki0423@gmail.com>
2025-06-24 13:41:31 +0900

    ABC411_A.cs

---
+++ ABC411_A.cs
@@ -0,0 +1,17 @@
+class ABC411_A
+{
+    static void Main()
+    {
+        string P = Console.ReadLine();
+        int L = int.Parse(Console.ReadLine());
+
+        if (P.Length >= L)
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
