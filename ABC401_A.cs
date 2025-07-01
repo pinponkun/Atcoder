bc7a4f145e9c876efac97d19b4a92df69b9edfd1
pinponkun <hayashidareki0423@gmail.com>
2025-07-01 17:22:25 +0900

    ABC401_A.cs

---
+++ ABC401_A.cs
@@ -0,0 +1,16 @@
+class ABC401_A
+{
+    static void Main()
+    {
+        int S = int.Parse(Console.ReadLine());
+
+        if (S >= 200 && S <= 299)
+        {
+            Console.WriteLine("Success");
+        }
+        else
+        {
+            Console.WriteLine("Failure");
+        }
+    }
+}
