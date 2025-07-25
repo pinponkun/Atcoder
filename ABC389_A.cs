86cd036d4cf845e135ae03da27a838c0f0da78ef
pinponkun <hayashidareki0423@gmail.com>
2025-07-25 20:21:55 +0900

    ABC389_A.cs

---
+++ ABC389_A.cs
@@ -0,0 +1,12 @@
+class ABC389_A
+{
+    static void Main()
+    {
+        string S = Console.ReadLine();
+
+        int a = S[0] - '0';
+        int b = S[2] - '0';
+
+        Console.WriteLine(a * b);
+    }
+}
